d989b82a47c0bdf1835f08cd101410271224127c
Michael <mrdayanghirang@gmail.com>
2022-10-02 17:18:26 -0700

    Need to figure out loop

--- javascript.js
+++ javascript.js
@@ -3,22 +3,26 @@ function getComputerChoice() {
    return choice[Math.floor(Math.random() * choice.length)];
 }
 
+const playerSelection = prompt('Make a Selection').toLowerCase();
+const computerSelection = getComputerChoice();
+
 function playRound(playerSelection, computerSelection) {
    if (playerSelection === computerSelection) {
        return 'Tie!';
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-       return "You win!";
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-       return "You win!";
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-       return "You win!";
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+       return 'You win! Rock beats Scissors';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+       return 'You win! Paper beats Rock';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+       return 'You win! Scissors beats Paper';
     } else {
        return `You lose!`;
     }
 }
 
-const playerSelection = prompt('Make a Selection').toLowerCase();
-const computerSelection = getComputerChoice();
+function game() {
+   for (let i = 0; i < 5; i++);
+}
 
 console.log('The Computer chose' + ' ' + computerSelection);
 console.log('You chose' + ' ' + playerSelection);
